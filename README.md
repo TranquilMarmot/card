9f0e49dbc3b5cf19dda82a40ca4b72fe090eb541
TranquilMarmot <nate272@gmail.com>
2019-11-30 23:56:32 -0800

    Card

---
+++ README.md
@@ -0,0 +1,35 @@
+# card
+
+## Running
+
+Via `npx`:
+
+```sh
+npx github:tranquilmarmot/card
+```
+
+Or locally:
+
+```sh
+npm install && npm run show
+```
+
+## Building
+
+```sh
+npm run build
+```
+
+This will overwrite [`bin/card`](bin/card) with a whatever is defined in [`build.ts`](build.ts)
+
+Running
+
+```sh
+./bin/show-card
+```
+
+Simply prints out whatever is in the [`bin/card`](bin/card) file.
+
+---
+
+Inspired by [bitandbang's card](https://github.com/bnb/bitandbang)
---
+++ build.ts
@@ -0,0 +1,94 @@
+import fs from "fs";
+import path from "path";
+import boxen, { BorderStyle } from "boxen";
+import chalk from "chalk";
+
+/** Create a colored label string */
+const label = (labelName: string): string => chalk.white.bold(`${labelName}:`);
+
+/** Create a colored URL string */
+const url = (baseUrl: string): string => chalk.grey(baseUrl);
+
+/** Create a colored symbol string (for i.e. @, /, -, .) */
+const sym = (symbol: string): string => chalk.magenta(symbol);
+
+/** A colored dot. */
+const dot = sym(".");
+
+/** My first name */
+const nate = chalk.cyan(`nate`);
+
+/** My last name */
+const moore = chalk.blue(`moore`);
+
+/** My handle */
+const tranquilMarmot = chalk.green("TranquilMarmot");
+
+/** Header for the card */
+const header = `           ${chalk.cyan.bold("Nate")} ${chalk.blue.bold(
+  "Moore"
+)} ${sym("/")} ${tranquilMarmot}`;
+
+/** Where I (currently) work */
+const work = `  ${chalk.white.bold("Engineering Manager")} at ${chalk.blue.dim(
+  "Tyler"
+)} ${chalk.green.dim("Technologies")}
+  ${chalk.cyan("Socrata")}, Data & Insights Division`;
+
+/** Where I (currently) live */
+const location = `  ${chalk.grey("Seattle, WA")}`;
+
+/** Where to reach me by electronic mail */
+const email = `   ${label("Email")}   ${nate}${sym("@")}${moore}${dot}codes`;
+
+/** Where to find my site on the web */
+const web = `     ${label("Web")}   ${chalk.grey(
+  "https://"
+)}${nate}${dot}${moore}${dot}codes`;
+
+/** Link to my GitHub profile */
+const github = `  ${label("GitHub")}   ${url(
+  "https://github.com/"
+)}${tranquilMarmot}`;
+
+/** Link to my Twitter account (it's mostly bad retweets) */
+const twitter = ` ${label("Twitter")}   ${url(
+  "https://twitter.com/"
+)}${tranquilMarmot}`;
+
+/** Link to my LinkedIn account (it's mostly out-of-date) */
+const linkedIn = `${label("LinkedIn")}   ${url(
+  "https://www.linkedin.com/in/"
+)}${nate}${sym("-")}${moore}`;
+
+/** The command you can run to show this card! */
+const cardCommand = `    ${label("Card")}   ${sym("npx")} ${sym(
+  "@"
+)}${tranquilMarmot}${sym("/")}card`;
+
+/** This is the actual string of the card... */
+const card = `${header}
+
+${work}
+${location}
+
+${email}
+${web}
+${github}
+${twitter}
+${linkedIn}
+
+${cardCommand}`;
+
+/** And this is the card surrounded by a nice box */
+const boxed = boxen(card, {
+  padding: 1,
+  margin: 1,
+  borderColor: "#6e9c78",
+  borderStyle: BorderStyle.Round
+});
+
+// write the file out to bin/card... bin/show_card will read and print it
+(async () => {
+  await fs.promises.writeFile(path.join(__dirname, "bin/card"), `${boxed}`);
+})();
